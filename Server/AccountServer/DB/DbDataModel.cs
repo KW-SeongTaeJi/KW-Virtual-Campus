07aa899accc748ca682872921a4e5956bcda2b86
KIMMINTAE98 <rlaaslxo98@naver.com>
2022-03-05 17:10:03 +0900

    Add : 로그인 용 AccountServer 추가

---
+++ Server/AccountServer/DB/DbDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountServer.DB
+{
+    [Table("Account")]
+    public class AccountDB
+    {
+        public int AccountDBId { get; set; }
+        public string AccountId { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Server/AccountServer/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using AccountServer.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class Extensions
+{
+    public static bool SaveChangesEx(this AppDbContext db)
+    {
+        try
+        {
+            db.SaveChanges();
+            return true;
+        }
+        catch
+        {
+            Console.WriteLine("Error: fail to save DB");
+            return false;
+        }
+    }
+}
