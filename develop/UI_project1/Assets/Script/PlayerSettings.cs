0a4458a47e39a4d88fb43e014ee272189dfa9e01
KIMMINTAE98 <rlaaslxo98@naver.com>
2022-03-09 01:12:11 +0900

    Add : UI 개발용 프로젝트 재추가

---
+++ develop/UI_project1/Assets/Script/PlayerSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse
+}
+public class PlayerSettings
+{
+    public static EControlType controlType;
+}
