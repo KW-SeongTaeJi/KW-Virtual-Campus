bad86b4530845256550067d0471269ae0165cb39
kim sunggil <sunggil97@naver.com>
2022-03-09 00:07:30 +0900

    feat: 이전 저장소로부터 unity 프로젝트파일 이전

---
+++ develop/UI_project1/Assets/Script/Window_Open/Friends_UI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class Friends_UI : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
+    }
+
+    private IEnumerator CloseAfterDelay()
+    {
+        animator.SetTrigger("close");
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+        animator.ResetTrigger("close");
+    }
+}
